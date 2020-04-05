572337b36ab6f16334b91e025d545fc3bda85f94
gabrielrodrigues5678 <63113861+gabrielrodrigues5678@users.noreply.github.com>
2020-04-05 16:40:36 -0300

    Create progesãoaritimedica.java

---
+++ progesãoaritimedica.java
@@ -0,0 +1,19 @@
+import java.uttil.Scanner;
+
+public class MyClass {
+    
+    public static void main(String args[]){
+        
+      int[] numeros = new int[](1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30);
+      
+      int gabril = 0; 
+      
+      for(int i=0; i<31; i++ ){
+          numeros[i] + gabriel = gabriel;}
+          
+          Systen.out.println ();
+          i++;
+        }
+      
+    }
+   
